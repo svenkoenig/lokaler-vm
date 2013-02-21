7c7f6e155b5bb6943c64c33138c1aa62ef29a139
Sven König <sven@popmodernism.org>
2013-02-21 18:41:15 +0100

    updated

--- local/settings.sh
+++ local/settings.sh
@@ -1,6 +1,7 @@
 PATH_REPOS_ALL=$HOME/repositories
 PATH_REPOS_LOCAL=$HOME/repositories/.local 
 PATH_REPOS_REMOTE=$HOME/repositories/.remote
+SETTING_LOKALER_ACTIVE_INSTANCE_FILE=$HOME/.active_instance
 
 LOGIN=sven@89.238.64.39
 
