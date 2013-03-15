a5b37367f73cdeef68be5ab2575e6f3082bfbe44
Sven König <sven@popmodernism.org>
2013-03-15 14:30:18 +0100

    updated

--- local/database/restore-latest-dump.sh
+++ local/database/restore-latest-dump.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+then
+	echo "missing argument destination database name."
+    exit 1
+else
+	dst_db_name=$1
+fi
+
 SETTINGS="`dirname \"$0\"`/db_settings.sh"
 source $SETTINGS
 
@@ -9,7 +17,7 @@ dump_file=`ls -tr $path_local_dumps/*-remote.tar.gz | tail -1`
 dump_file=`basename $dump_file | sed 's/\(.*\)\..*/\1/' | sed 's/\(.*\)\..*/\1/'`
 echo "dumpfile: $dump_file"
 
-source $RESTORE_DUMP $dump_file 2> /dev/null
+source $RESTORE_DUMP $dump_file $dst_db_name 2> /dev/null
 
 $MANAGE migrate
 $MANAGE create_or_reset_admin
--- local/database/restore_dump.sh
+++ local/database/restore_dump.sh
@@ -5,7 +5,14 @@
 if [ -n "$1" ]; then
     dump_file=$1
 else
-    echo "missing argument."
+    echo "missing argument dump file."
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    dst_database_name=$2
+else
+    echo "missing argument destination database name."
     exit 1
 fi
 
@@ -18,7 +25,6 @@ if [ -f "$dump_file.tar.gz" ]; then
   echo "decompressing dump..."
   cmd="gzip -dc \"$dump_file.tar.gz\" > \"$dump_file.tar\""
   echo "cmd: $cmd"
-  # $cmd
   gzip -dc "$dump_file.tar.gz" > "$dump_file.tar"
 fi
 
@@ -29,11 +35,11 @@ then
         echo "there are clients connected to the database. please disconnect them first."
     else
         echo "dropping and recreating database..."
-		dropdb $database_name
-		createdb $database_name -T template_postgis
+		dropdb $dst_database_name
+		createdb $dst_database_name -T template_postgis
         echo "restoring dump..."
-        echo '*' pg_restore -d $database_name $dump_file.tar
-        pg_restore -d $database_name $dump_file.tar
+        echo '*' pg_restore -d $dst_database_name $dump_file.tar
+        pg_restore -d $dst_database_name $dump_file.tar
         date > db_restore-info.txt
         echo "cleaning up..."
         rm "$dump_file.tar"
